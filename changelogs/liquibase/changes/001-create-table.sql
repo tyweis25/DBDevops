8d08885c92f2d0068955d190142973a413366955
ty.weis@harness.io <ty.weis@harness.io>
2026-04-15 23:31:50 +0000

    Edit changelogs/liquibase/changes/001-create-table.sql

--- changelogs/liquibase/changes/001-create-table.sql
+++ changelogs/liquibase/changes/001-create-table.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 --changeset Ty Weis:1
-CREATE TABLE test_table (
+CREATE TABLE dbdevops.test_table (
     id INT NOT NULL IDENTITY(1,1),
     name NVARCHAR(50) NOT NULL,
     created_at DATETIME DEFAULT GETDATE(),
