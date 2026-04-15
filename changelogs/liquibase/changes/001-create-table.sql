979871378e311c7828c1e6c58665318e9511b9be
ty.weis@harness.io <ty.weis@harness.io>
2026-04-15 23:33:54 +0000

    Create changelogs/liquibase/changes/001-create-table.sql

---
+++ changelogs/liquibase/changes/001-create-table.sql
@@ -0,0 +1,10 @@
+--liquibase formatted sql
+
+--changeset Ty Weis:1
+CREATE TABLE dbdevops.test_table (
+    id INT NOT NULL IDENTITY(1,1),
+    name NVARCHAR(50) NOT NULL,
+    created_at DATETIME DEFAULT GETDATE(),
+    PRIMARY KEY (id)
+);
+--rollback DROP TABLE test_table;
